65ae99f236e4ab20c6827d6b770706a6851b41e6
jkorovesi <joshua.j.korovesi@hotmail.com>
2017-04-06 01:50:50 -0400

    stuff

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,18 @@
+/**
+ * File name: index.js
+ * Authors:
+ * Shawn McLaughlin <shawnmcdev@gmail.com>
+ * Joshua Korovesi <joshua.j.korovesi@gmail.com>
+ * Site: https://joshawn-opine.herokuapp.com/
+ * Description: Defines index routes.
+ */
+
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home' });
+  res.render('./index/home', { title: 'Home' });
 });
 
 module.exports = router;
